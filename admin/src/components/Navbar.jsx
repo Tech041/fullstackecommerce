5feafd6ceb7e36b26c099ba431df4a65987f5b88
nelson <chinwubanelson@gmail.com>
2025-02-13 14:14:23 +0100

    updated pages

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets";
 const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
-      <img src={assets.logo} alt="" className="w-[max(10%,80px)]" />
+      <img src={assets.nelpharm_logo} alt="" className="w-[max(10%,80px)]" />
       <button
         onClick={() => setToken("")}
         className=" bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
