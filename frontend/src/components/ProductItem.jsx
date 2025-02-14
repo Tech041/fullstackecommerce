4d1a9f50de6c93129224ed9aa131d0578c7f27f3
nelson <chinwubanelson@gmail.com>
2025-02-14 08:09:39 +0100

    edited

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -6,7 +6,11 @@ const ProductItem = ({ id, image, name, price }) => {
   const { currency } = useContext(ShopContext);
 
   return (
-    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+    <Link
+      className="text-gray-700 cursor-pointer"
+      to={`/product/${id}`}
+      onClick={() => scrollTo(0, 0)}
+    >
       <div className="overflow-hidden">
         <img
           src={image[0]}
